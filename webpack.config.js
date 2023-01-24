833233dd4b3808826c0f4d26c1fef190d5e8c993
KoMMentatoR1 <leshasherbina@gmail.com>
2023-01-24 21:50:58 +0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/mora-front/',
+    publicPath: '/',
   },
   mode: 'development',
   devServer: {
