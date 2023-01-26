369f3fd79ade0e932879739b84c7d4bbd8ddfe6d
KoMMentatoR1 <leshasherbina@gmail.com>
2023-01-26 19:07:55 +0300

    fix: delete bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
